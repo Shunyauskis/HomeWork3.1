e36cb2903a8d01f23c60bb6badb7ce5480cb6d50
Shunyauskis <apismenniy@gmail.com>
2021-11-09 11:34:09 +0200

    Initial commit

---
+++ src/com/gmail/apismenniy/Group.java
@@ -0,0 +1,86 @@
+package com.gmail.apismenniy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Group {
+    private Student [] students = new Student[10];
+    private String groupName;
+
+    public Group() {
+        super();
+    }
+    public Group(String groupName){
+        this.groupName = groupName;
+    }
+
+    public Group(Student[] students, String groupName) {
+        this.students = students;
+        this.groupName = groupName;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public String toString() {
+        String student = "";
+
+        for (int i = 0; i<students.length; i++){
+            student = student + students[i] + '\n';
+        }
+        return groupName + '\n' + student;
+    }
+
+    public void addStudent(Student student) throws GroupOverflowException{
+        int counter = 1;
+        for (int i = 0; i<students.length; i++) {
+            if (students[i] == null) {
+                students[i] = student;
+                break;
+            } counter++;
+            if (counter>students.length) {
+                throw new GroupOverflowException("The group is full");
+                }
+            }
+        }
+
+
+    public Student deleteStudent(long id) {
+        for (int i = 0; i<students.length; i++){
+            if(students[i].getId() == id){
+                students[i] = null;
+            }
+        }
+        return null;
+    }
+
+    public Student searchByLastName(String lastName) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] != null && lastName.equals(students[i].getLastName())) {
+                return students[i];
+            }
+
+        }return null;
+    }
+
+
+    public void sortStudentsByLastName(){
+        Arrays.sort(students, Comparator.nullsFirst (new SortStudentsByLastNameComparator()));
+    }
+
+}
+
---
+++ src/com/gmail/apismenniy/Main.java
@@ -0,0 +1,47 @@
+package com.gmail.apismenniy;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws GroupOverflowException {
+
+        Student studentOne = new Student("Petya", "Petechkin", Gender.MALE, "Economical", 566781L);
+        Student studentTwo = new Student("Vasya", "Vasichkin", Gender.MALE, "Economical", 566782L);
+        Student studentThree = new Student("Valera", "Kovalskiy", Gender.MALE, "Economical", 566783L);
+        Student studentFour = new Student("Masha", "Rasputina", Gender.FEMALE, "Economical", 566784L);
+        Student studentFive = new Student("Silvestr", "Stallone", Gender.MALE, "Economical", 566785L);
+        Student studentSix = new Student("Megan", "Fox", Gender.FEMALE, "Economical", 566786L);
+        Student studentSeven = new Student("Antonio", "Banderos", Gender.MALE, "Economical", 566787L);
+        Student studentEight = new Student("Khristina", "Orbakayte", Gender.FEMALE, "Economical", 566788L);
+        Student studentNine = new Student("Mike", "Tyson", Gender.MALE, "Economical", 566789L);
+        Student studentTen = new Student("Pamela", "Anderson", Gender.FEMALE, "Economical", 566790L);
+        Student studentEleven = new Student("Alla", "Pugacheva", Gender.FEMALE, "Economical", 566791L);
+        Student studentTwelve = new Student("Denzel", "Washington", Gender.MALE, "Economical", 566792L);
+        Group economical = new Group("economical");
+
+        economical.addStudent(studentOne);
+        economical.addStudent(studentTwo);
+        economical.addStudent(studentThree);
+        economical.addStudent(studentFour);
+        economical.addStudent(studentFive);
+        economical.addStudent(studentSix);
+        economical.addStudent(studentSeven);
+        economical.addStudent(studentEight);
+        economical.addStudent(studentNine);
+        economical.addStudent(studentTen);
+//        economical.addStudent(studentEleven);
+//        economical.addStudent(studentTwelve);
+
+        System.out.println();
+        System.out.println(economical.toString());
+
+        economical.sortStudentsByLastName();
+        System.out.println(economical.toString());
+
+        System.out.println(economical.searchByLastName("Anderson"));
+
+        economical.deleteStudent(566790);
+        System.out.println(economical.toString());
+    }
+}
